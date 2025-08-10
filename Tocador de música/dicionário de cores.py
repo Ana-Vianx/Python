062a2f6580e78e7db9ffe06a5d0dbfb3c2912e7b
Ana Gabriela Viana <anagabrielavianna@gmail.com>
2025-08-10 16:17:41 -0300

    Add files via upload

---
+++ Tocador de música/dicionário de cores.py
@@ -0,0 +1,41 @@
+print("""Tabela de cores: 
+\033[1;30mPreto	
+\033[1;31mVermelho	
+\033[1;32mVerde
+\033[1;33mAmarelo
+\033[38;5;208mLaranja	
+\033[1;34mAzul	
+\033[1;35mMagenta	
+\033[1;36mCyan	
+\033[1;37mCinza Claro	
+\033[1;90mCinza Escuro		
+\033[1;91mVermelho Claro	
+\033[1;92mVerde Claro	
+\033[1;93mAmarelo Claro	
+\033[1;94mAzul Claro	
+\033[1;95mMagenta Claro	
+\033[1;96mCyan Claro	
+\033[38;2;255;105;180mRosa
+\033[1;97mBranco		
+\033[;1mNegrito	
+\033[;7mInverte	 \033[m
+\033[0;0mReset (remove formatação)		
+""")
+print("""Tabela de fundos: 
+\033[1;40mPreto	\033[0m
+\033[1;41mVermelho\033[0m
+\033[1;42mVerde\033[0m
+\033[1;43mAmarelo\033[0m
+\033[1;44mAzul\033[0m
+\033[1;45mMagenta\033[0m
+\033[1;46mCyan\033[0m
+\033[1;47mBranco\033[0m
+\033[1;100mCinza Escuro\033[0m
+\033[1;101mVermelho Claro\033[0m
+\033[1;102mVerde Claro\033[0m
+\033[1;103mAmarelo Claro\033[0m
+\033[1;104mAzul Claro\033[0m
+\033[1;105mMagenta Claro\033[0m
+\033[1;106mCyan Claro\033[0m
+\033[1;107mBranco\033[0m
+""")
